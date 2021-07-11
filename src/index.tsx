3ec2bc79a825191076aa992200292c624102398f
John M. Wargo <johnwargo@users.noreply.github.com>
2021-07-11 10:12:58 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 
 import App from './App';
+import './index.css';
 
 ReactGA.initialize('G-L3V93PGRQH');
 
